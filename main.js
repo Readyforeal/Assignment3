5147dc32bf2773d3f2916d3a3bb1e73b05090881
Jamie Henrickson <53891976+Readyforeal@users.noreply.github.com>
2019-12-14 15:07:51 -0700

    Fixed final correct answer count

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
     var totalQuestions = 15;
     var correctAnswers = 0;
     var questionsAnswered = 0;
-
-    var completedMessage = "Congrats! You finished with " + correctAnswers + " out of 15 correct.";
-    var tooSlowMessage = "Time's up. You finished with " + correctAnswers + " out of 15 correct. Thank you for participating.";
+    var timeUp = false;
 
     $("#submit").on("click", function(){
-        ValidateAnswers(completedMessage);
+        ValidateAnswers();
     });
 
     //Progress bar
@@ -46,7 +44,8 @@ $(document).ready(function(){
             display.innerHTML = minutes + ":" + seconds;
 
             if(minutes == 0 && seconds == 0){
-                ValidateAnswers(tooSlowMessage);
+                timeUp = true;
+                ValidateAnswers();
             }
 
         },1000);
@@ -54,7 +53,7 @@ $(document).ready(function(){
 
     function Advance(){
 
-        var progressbarWidth = 0
+        var progressbarWidth = 0;
 
         for(var i = 1; i < totalQuestions; i++){
             var questionAnswers = document.getElementsByName("q" + i + "-answer");
@@ -75,54 +74,58 @@ $(document).ready(function(){
         $("#progressbar").css("width", progressbarWidth + "%");
     }
 
-    function ValidateAnswers(message){
+    function ValidateAnswers(){
         if(document.getElementById("a3").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a6").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a9").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a16").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a20").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a23").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a28").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a30").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a36").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a37").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a41").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a44").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a46").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("a47").checked == true){
-            correctAnswers++;
+            correctAnswers += 1;
         }
         if(document.getElementById("text-answer").value == "15"){
-            correctAnswers++;
+            correctAnswers += 1;
         }
 
-        alert(message);
+        if(!timeUp){
+            alert("Congrats! You finished with " + correctAnswers + " out of 15 correct.");
+        }else{
+            alert("Time's up. You finished with " + correctAnswers + " out of 15 correct. Thank you for participating.");
+        }
 
         window.location.href = "index.html";
     }
